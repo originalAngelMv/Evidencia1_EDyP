7b575b8efed9aa5cd8bb56c7981b5a21a757b73d
originalAngelMv <143576121+originalAngelMv@users.noreply.github.com>
2023-08-31 00:59:37 -0600

    Agrega opcion 2 y su subconsulta opcion 1.

--- Evidencia1.py
+++ Evidencia1.py
@@ -37,6 +37,47 @@ while True:
         notas.append(nota)
         print(f"Folio asignado: {folios_contador}")
     
+    elif opcion == "2":
+        print("1. Consultar por período.\n2. Consultar por folio.")
+        consulta_opcion = input("Ingrese una opción de consulta:\n")
+        
+        if consulta_opcion == "1":
+            
+            fecha_inicial_str = input("Fecha inicial (dd-mm-aaaa): ")
+            fecha_final_str = input("Fecha final (dd-mm-aaaa): ")
+            fecha_inicial = datetime.datetime.strptime(fecha_inicial_str, "%d-%m-%Y")
+            fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
+            
+            notas_en_periodo = []
+            
+            for nota in notas:
+                if nota['fecha']>=fecha_inicial and nota['fecha']<= fecha_final and nota['estado']:
+                    notas_en_periodo.append(nota)
+                    
+            if notas_en_periodo:
+                
+                print('\nNotas encontradas en el período:')
+                print("-"*50)
+                
+                for nota in notas_en_periodo:
+                    
+                    print(f"Folio: {nota['folio']}\t Cliente: {nota['cliente']}\t Fecha: {nota['fecha'].strftime('%d-%m-%Y')}")
+                    for servicio,costo in nota['detalle']:
+                        print(f' Servicio: {servicio}-----> Costo: {costo:.2f}')
+                    print(f'Monto: {nota["monto_a_pagar"]:.2f}')
+                    print("-"*50)
+                else:
+                    print('Fin de las notas')
+            else:
+                print('\nNo se encontraron notas en el período especificado')
+                
+        elif opcion == "2":
+            pass
+                
+                 
+            
+                
+    
             
             
         
