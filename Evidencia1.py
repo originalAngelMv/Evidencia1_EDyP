5e51d6ad8870d591bb931833b235d11b49164aa0
originalAngelMv <143576121+originalAngelMv@users.noreply.github.com>
2023-09-01 20:14:05 -0600

    Se mejoro la opcion 1 y 2 y la primera consulta de la opcion 2, y se pusieron validaciones

--- Evidencia1.py
+++ Evidencia1.py
@@ -3,7 +3,11 @@ import re
 
 notas = []
 folios_contador = 0
+
+patron_fecha = r"^\d{1,2}-\d{2}-\d{4}"
+
 while True:
+    
     print("1. Registrar una nota.\n2. Consultas y reportes.\n3. Cancelar una nota.\n4. Recuperar nota.\n5. Salir")
     
     opcion = input("Ingrese una opción:\n")
@@ -12,10 +16,41 @@ while True:
         
         folios_contador += 1
         
-        cliente = input("Nombre del cliente:\n")
+        while True:
         
-        fecha_ingresada_str = input("Fecha de la nota (dd-mm-aaaa):\n")
-        fecha_ingresada = datetime.datetime.strftime(fecha_ingresada_str, "%d-%m-%Y")
+            cliente = input("Nombre del cliente:\n")
+            
+            if cliente.strip()=="":
+                print("El dato no puede omitirse. Intente nuevamente")
+                continue
+            else:
+                break
+        
+        fecha_actual = datetime.datetime.now()
+        
+        while True:
+            
+            fecha_ingresada_str = input("Fecha de la nota (dd-mm-aaaa):\n")
+            
+            if fecha_ingresada_str =="":
+                print("El dato no puede omitirse. Intente nuevamente.")
+                continue
+            elif not re.match(patron_fecha,fecha_ingresada_str):
+                print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa")
+                continue
+            
+            
+            try:
+                fecha_ingresada = datetime.datetime.strftime(fecha_ingresada_str, "%d-%m-%Y")
+            except Exception:
+                print("La fecha NO existe. Intente nuevamente.")
+                continue
+            
+            if fecha_ingresada>fecha_actual:
+                print("No debe ser posterior a la fecha actual del sitema. Intente nuevamente.")
+                continue
+            else:
+                break
         
         nota = {
             "folio": folios_contador,
@@ -28,51 +63,115 @@ while True:
         
         while True:
             
-            nombre_servicio = input("Nombre del servicio: ")
-            costo_servicio = input("Costo del servicio: ")
-            costo_servicio = float(costo_servicio)
+            while True:
             
-            if input("¿Agregar otro servicio? (s/n): ").lower() != "s":
+                nombre_servicio = input("Nombre del servicio: ")
+                
+                if nombre_servicio.strip()=="":
+                    print("El dato no puede omitirse. Intente nuevamente.")
+                    continue
+                break
+                
+            while True:
+                
+                costo_servicio = input("Costo del servicio: ")
+                
+                if costo_servicio.strip()=="":
+                    print("El dato no puede omitirse. Intente nuevamente.")
+                    continue
+                
+                try:
+                    costo_servicio = float(costo_servicio)
+                except Exception:
+                    print("Se ingreso un carácter no númerico ")
+                    continue
+                
+                if costo_servicio<=0:
+                    print("El costo debe ser mayor a 0 pesos. Intente nuevamente.")
+                    continue
+                else:
+                    nota['detalle'].append((nombre_servicio,costo_servicio))
+                    nota['monto_a_pagar'] += costo_servicio
+                    break
+            
+            if input("¿Agregar otro servicio? (s/n): \n").lower() != "s":
                 break
             
         notas.append(nota)
-        print(f"Folio asignado: {folios_contador}")
+        print(f"\nFolio asignado: {folios_contador}\n")
+        print(f"Nombre: {cliente}")
+        print(f"Fecha: {fecha_ingresada.date()}\n")
+        print("Detalles: ")
+        for servicio,costo in nota['detalle']:
+            print(f"servicio: {servicio}---->costo: {costo}")
+        print(f"Monto total a pagar: {nota['monto_a_pagar']:.2f}")
+    
+    
     
     elif opcion == "2":
-        print("1. Consultar por período.\n2. Consultar por folio.")
-        consulta_opcion = input("Ingrese una opción de consulta:\n")
+        print("\n1. Consultar por período.\n2. Consultar por folio.")
+        consulta_opcion = input("\nIngrese una opción de consulta:\n")
         
         if consulta_opcion == "1":
             
-            fecha_inicial_str = input("Fecha inicial (dd-mm-aaaa): ")
-            fecha_final_str = input("Fecha final (dd-mm-aaaa): ")
-            fecha_inicial = datetime.datetime.strptime(fecha_inicial_str, "%d-%m-%Y")
-            fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
-            
+            while True:
+                
+                fecha_inicial_str = input("Fecha inicial (dd-mm-aaaa): ")
+                
+                if fecha_inicial_str =="":
+                    print("El dato no puede omitirse.Intente denuevo.")
+                    continue
+                elif not re.match(patron_fecha, fecha_inicial_str):
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa")
+                    continue
+                
+                try:
+                    fecha_inicial = datetime.datetime.strptime(fecha_inicial_str, "%d-%m-%Y")
+                except Exception:
+                    print("La fecha NO existe. Intente denuevo.")
+                    continue
+                else:
+                    break
+                
+            while True:
+                
+                fecha_final_str = input("Fecha final (dd-mm-aaaa): ")
+                
+                if fecha_final_str =="":
+                    print("El dato no puede omitirse.Intente denuevo.")
+                    continue
+                elif not re.match(patron_fecha, fecha_final_str):
+                    print("Formato de fecha incorrecto. Debe ser dd-mm-aaaa")
+                    continue
+                
+                try:
+                    fecha_final = datetime.datetime.strptime(fecha_final_str, "%d-%m-%Y")
+                except Exception:
+                    print("La fecha NO existe. Intente denuevo.")
+                    continue
+                else:
+                    break
+
             notas_en_periodo = []
             
             for nota in notas:
+                
                 if nota['fecha']>=fecha_inicial and nota['fecha']<= fecha_final and nota['estado']:
                     notas_en_periodo.append(nota)
                     
             if notas_en_periodo:
-                
                 print('\nNotas encontradas en el período:')
-                print("-"*50)
+                print("-"*100)
                 
                 for nota in notas_en_periodo:
-                    
                     print(f"Folio: {nota['folio']}\t Cliente: {nota['cliente']}\t Fecha: {nota['fecha'].strftime('%d-%m-%Y')}")
-                    for servicio,costo in nota['detalle']:
-                        print(f' Servicio: {servicio}-----> Costo: {costo:.2f}')
                     print(f'Monto: {nota["monto_a_pagar"]:.2f}')
-                    print("-"*50)
+                    print("-"*100)
                 else:
                     print('Fin de las notas')
             else:
                 print('\nNo se encontraron notas en el período especificado')
-                
-        elif opcion == "2":
+        elif consulta_opcion == "2":
             pass
                 
                  
